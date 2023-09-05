6a2f67643603ccac4a9a06c4476f37301d8c127c
yihengx <yihengx66@gmail.com>
2023-09-04 17:21:24 -0700

    setup skeleton

--- sphinx_versioning/sphinx_versioning.py
+++ sphinx_versioning/sphinx_versioning.py
@@ -5,25 +5,10 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger(__name__)
 
 
-TEMPLATE_CONTENT_LATEST_BUILD = """{% if sphinx_versions %}
-    <span style="vertical-align: middle;">{{ _('Versions') }}</span>
-    <select style="vertical-align: middle; margin-left: 5px;" onchange="window.location.href=this.value" id="sphinx_versioning_dropdown_menu">
-        <option value="/" selected>Latest</option>
-        {%- for item in sphinx_versions %}
-            <option value="{{ pathto('_static/sphinx_versioning_plugin/{}'.format(item), 1) }}">{{ item }}</option>
-        {%- endfor %}
-    </select>
-{% endif %}
-"""
-
-TEMPLATE_CONTENT_VERSION_BUILD = """<span style="vertical-align: middle;">{{ _('Versions') }}</span>
-    <select style="vertical-align: middle; margin-left: 5px;" onchange="window.location.href=this.value" id="sphinx_versioning_dropdown_menu">
-        <option value="/">Latest</option>
-    </select>
-"""
+HOME_PAGE_TEMPLATE = """"""
 
 
-def write_template_file_for_latest_build(app):
+def latest_build_setup(app):
     """
     Write the template file for the latest build. The build should be triggered by `sphinx build`.
     The template should have link to all the versions available.
@@ -43,7 +28,7 @@ def write_template_file_for_latest_build(app):
         f.write(TEMPLATE_CONTENT_LATEST_BUILD)
 
 
-def write_template_file_for_version_build(app):
+def version_build_setup(app):
     """
     Write the template file for the version build. The build should be triggered by `sphinx-version -v <version>`.
     The template should only have link to the latest version.
@@ -68,78 +53,21 @@ def get_version_list(app):
     return sorted(subdirs, reverse=True)  # Assuming you'd like the versions sorted in descending order
 
 
-def update_sidebar_links_for_versioned_docs(versions_dir, versions):
-    """
-    Update all the .html files under each version in 
-    `_static/sphinx_versioning_plugin/` with the available versions.
-    """
-    for version in versions:
-        for root, dirs, files in os.walk(os.path.join(versions_dir, version)):
-            for file in files:
-                if file.endswith('.html'):
-                    file_path = os.path.join(root, file)
-                    file_modified = False  # Track if the file was modified
-                    
-                    try:
-                        with open(file_path, 'r') as f:
-                            soup = BeautifulSoup(f, 'html.parser')
-
-                            # Find the select tag with the specified id
-                            select_tag = soup.find("select", {"id": "sphinx_versioning_dropdown_menu"})
-
-                            if select_tag:
-                                logger.info(f"Updating version dropdown for {version} in {file}")
-                                select_tag.clear()  # Clear existing options
-
-                                option_latest = soup.new_tag("option", value="/")
-                                option_latest.string = "Latest"
-                                select_tag.append(option_latest)
-
-                                for v in versions:
-                                    option = soup.new_tag("option", value=f"../{v}")
-                                    option.string = v
-
-                                    # Mark as selected if it's the current version
-                                    if v == version:
-                                        option.attrs["selected"] = "selected"
-
-                                    select_tag.append(option)
-                                
-                                file_modified = True
-
-                        # Only write back to the file if it was modified
-                        if file_modified:
-                            with open(file_path, 'w') as f:
-                                f.write(str(soup))
-
-                    except Exception as e:
-                        logger.error(f"Error processing {file_path}. Error: {e}")
-
-
-def generate_versioning_sidebar(app, config):
+def versioning_build(app, config):
     
     sphinx_versions_env = os.environ.get("SPHINX_VERSIONING_PLUGIN")
     
     if sphinx_versions_env == "1":
         logger.info("Versioned docs build")
-        write_template_file_for_version_build(app)
+        version_build_setup(app)
         return
 
-    # write the template file
-    write_template_file_for_latest_build(app)
+    # Set up for the latest build
+    latest_build_setup(app)
 
     # Get versions from the directory structure
     sphinx_versions = get_version_list(app)
-    
-    # Update the sidebar links for versioned docs
-    versions_dir = os.path.join(app.srcdir, "_static", "sphinx_versioning_plugin")
-
-    update_sidebar_links_for_versioned_docs(versions_dir, sphinx_versions)
-
-    # update html_context with versions
-    app.config.html_context.update({"sphinx_versions": sphinx_versions})
-
 
 def setup(app):
 
-    app.connect("config-inited", generate_versioning_sidebar)
+    app.connect("config-inited", versioning_build)
